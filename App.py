9a38fcc8498ed289bf8f945d54102f4b36866f20
L4ncelot-R <wrongqir@icloud.com>
2023-06-05 10:10:08 +0800

    minor bug fix

--- App.py
+++ App.py
@@ -48,6 +48,9 @@ def get_drop(event, filetype):
     file_path = event.data
     if os.path.isfile(file_path):
         _, ext = os.path.splitext(file_path)
+    elif file_path.endswith('}') and os.path.isfile(file_path[1:-1]):
+        file_path = file_path[1:-1]
+        _, ext = os.path.splitext(file_path)
     else:
         messagebox.showerror("Error", f"Cannot open file from {file_path}")
         return
